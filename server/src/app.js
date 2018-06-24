3a075b20b31f59d5226404b038ad0af7f2e0b091
amitabh <buddhabacchan@gmail.com>
2018-06-24 15:04:17 -0700

    status route added

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,11 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(morgan('combine'))
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 app.use(cors())
+
+app.get('/status', (req, res) => {
+  return res.send({message: 'hello world'})
+})
 
 app.listen(process.env.PORT || 3000)
